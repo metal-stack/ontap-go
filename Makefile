f2d173f49be05b7b73d7ea9cf25c9ebf53008146
Stefan Majer <stefan.majer@f-i-ts.de>
2025-01-17 10:00:52 +0100

    Initial

---
+++ Makefile
@@ -0,0 +1,31 @@
+METAL_API_VERSION := $(or ${METAL_API_VERSION},$(shell cat VERSION))
+
+release:: generate-client mocks gofmt test;
+
+.PHONY: generate-client
+generate-client:
+	rm -rf api
+	mkdir -p api
+	docker run --rm \
+		--user $$(id -u):$$(id -g) \
+		-v ${PWD}:/work \
+		-w /work \
+		ghcr.io/metal-stack/builder swagger generate client -A Ontap -f spec/ontap.yaml -t api --struct-tags json --struct-tags yaml
+	rm api/models/application_template.go # this redeclares ApplicationTemplate which is a BUG in the ontap swagger spec.
+
+.PHONY: mocks
+mocks:
+	rm -rf test/mocks
+	docker run --rm \
+		--user $$(id -u):$$(id -g) \
+		-w /work \
+		-v ${PWD}:/work \
+		vektra/mockery:v2.51.0 -r --keeptree --inpackage --dir api/client --output test/mocks --all
+
+.PHONY: gofmt
+gofmt:
+	go fmt ./...
+
+.PHONY: test
+test:
+	go test ./... -coverprofile=coverage.out -covermode=atomic && go tool cover -func=coverage.out
---
+++ pkg/client/client.go
@@ -0,0 +1,44 @@
+package client
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"net/http"
+
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+
+	"github.com/metal-stack/ontap-go/api/client"
+)
+
+type Config struct {
+	AdminUser     string
+	AdminPassword string
+	Host          string
+	InsecureTLS   bool
+}
+
+func NewAPIClient(cfg Config) (*client.Ontap, error) {
+	transport := httptransport.New(cfg.Host, "/api", []string{"https"})
+
+	//  API call error: no consumer: "application/hal+json", need to fix this
+	jsonConsumer := runtime.JSONConsumer()
+	transport.Consumers["application/hal+json"] = jsonConsumer
+
+	// Insecure skip verify if needed just for testing
+	if httpTrans, ok := transport.Transport.(*http.Transport); ok && cfg.InsecureTLS {
+		httpTrans.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	// Basic Auth for now
+	userpass := base64.StdEncoding.EncodeToString([]byte(cfg.AdminUser + ":" + cfg.AdminPassword))
+	transport.DefaultAuthentication = runtime.ClientAuthInfoWriterFunc(
+		func(req runtime.ClientRequest, reg strfmt.Registry) error {
+			return req.SetHeaderParam("Authorization", "Basic "+userpass)
+		},
+	)
+
+	client := client.New(transport, strfmt.Default)
+	return client, nil
+}
