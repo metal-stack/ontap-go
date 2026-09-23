478372dcc93e048e4616f971324a2c6aa5feeeb4
Gerrit <tirreg91@googlemail.com>
2026-09-23 12:41:07 +0200

    Allow more client TLS config options like providing certs.

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,9 +1,9 @@
 package client
 
 import (
-	"crypto/tls"
-	"encoding/base64"
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
@@ -13,34 +13,73 @@ import (
 )
 
 type Config struct {
-	AdminUser     string
-	AdminPassword string
-	Host          string
-	InsecureTLS   bool
+	// ApiURL points to the ontap api endpoint, usually ends with "/api"
+	ApiURL string
+	// BasicAuth configures basic auth for api communication if provided
+	BasicAuth *BasicAuthConfig
+	// TLS sets client tls configuration if provided
+	TLS *TLSConfig
+}
+
+type BasicAuthConfig struct {
+	User     string
+	Password string
+}
+
+type TLSConfig struct {
+	CertPath    string
+	KeyPath     string
+	CaPath      string
+	InsecureTLS *bool
 }
 
 func NewAPIClient(cfg Config) (*client.Ontap, error) {
-	transport := httptransport.New(cfg.Host, "/api", []string{"https"})
-
-	//  API call error: no consumer: "application/hal+json", need to fix this
-	jsonConsumer := runtime.JSONConsumer()
-	transport.Consumers["application/hal+json"] = jsonConsumer
-
-	// Insecure skip verify if needed just for testing
-	if httpTrans, ok := transport.Transport.(*http.Transport); ok && cfg.InsecureTLS {
-		httpTrans.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //nolint:all
+	parsedURL, err := url.Parse(cfg.ApiURL)
+	if err != nil {
+		return nil, err
+	}
+	if parsedURL.Host == "" {
+		return nil, fmt.Errorf("invalid ontap api url: %s, must be in the form scheme://host[:port]/basepath", cfg.ApiURL)
 	}
 
-	// Basic Auth for now
-	userpass := base64.StdEncoding.EncodeToString([]byte(cfg.AdminUser + ":" + cfg.AdminPassword))
-	transport.DefaultAuthentication = runtime.ClientAuthInfoWriterFunc(
-		func(req runtime.ClientRequest, reg strfmt.Registry) error {
-			return req.SetHeaderParam("Authorization", "Basic "+userpass)
-		},
-	)
+	httpClient := http.DefaultClient
 
-	client := client.New(transport, strfmt.Default)
-	return client, nil
+	if cfg.TLS != nil {
+		tlsOptions := httptransport.TLSClientOptions{}
+
+		if cfg.TLS.CaPath != "" {
+			tlsOptions.CA = cfg.TLS.CaPath
+		}
+		if cfg.TLS.CertPath != "" {
+			tlsOptions.Certificate = cfg.TLS.CertPath
+		}
+		if cfg.TLS.KeyPath != "" {
+			tlsOptions.Key = cfg.TLS.KeyPath
+		}
+		if cfg.TLS.InsecureTLS != nil {
+			tlsOptions.InsecureSkipVerify = *cfg.TLS.InsecureTLS
+		}
+
+		tlsConfig, err := httptransport.TLSClientAuth(tlsOptions)
+		if err != nil {
+			return nil, err
+		}
+
+		httpClient.Transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	}
+
+	transport := httptransport.NewWithClient(parsedURL.Host, parsedURL.Path, []string{parsedURL.Scheme}, httpClient)
+
+	//  API call error: no consumer: "application/hal+json", need to fix this
+	transport.Consumers["application/hal+json"] = runtime.JSONConsumer()
+
+	if cfg.BasicAuth != nil {
+		transport.DefaultAuthentication = httptransport.BasicAuth(cfg.BasicAuth.User, cfg.BasicAuth.Password)
+	}
+
+	return client.New(transport, strfmt.Default), nil
 }
 
 // MetroClusterConfig holds the configuration for n clusters in a metro cluster.
